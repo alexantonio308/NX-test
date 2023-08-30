0e8b0c3edbfb79a4445ffae884d66c05a91fb9ae
Alex Antonio <alexantonio308@gmail.com>
2023-08-29 22:28:38 -0300

    Orde list component

---
+++ modules/orders/src/lib/order-list/order-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'org-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.scss'],
+})
+export class OrderListComponent {}
